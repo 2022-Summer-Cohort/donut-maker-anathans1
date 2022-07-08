f501bc0e366bbd3281f48581e136f3450f5758fc
anathans1 <alexnathans1993@gmail.com>
2022-07-08 13:45:37 -0400

    All 3 buttons functional but autoclicker has a delay

--- Donuts.js
+++ Donuts.js
@@ -3,6 +3,9 @@ class Donuts {
         this._count = count;
         this._autoclick = autoclick;
         this._multiplier = multiplier;
+        this._autoclickCost = 100;
+        this._multiplierCost = 10;
+        this._clickValue = 1
     }
 
     get count() {
@@ -12,18 +15,55 @@ class Donuts {
         this._autoclick;
     }
     get multiplier() {
-        this._multiplier
+        this._multiplier;
+    }
+    get autoclickCost() {
+        this._autoclickCost;
+    }
+    get multiplierCost() {
+        this._multiplierCost;
+    }
+    get clickValue() {
+        this._clickValue;
     }
 
+
     click() {
-        this._count++;
-     
+        this._count += this._clickValue;
     }
-    getStatus(){
-        return "Donuts: " + this._count + " Auto-Clickers: " + this.autoclick + " Multiplier: " + this._multiplier;
+    autoclickClick() {
+        if(this._count >= this._autoclickCost) {
+            this._count -= this._autoclickCost;
+            this._autoclickCost *= 1.1;
+            this._autoclick++;}
+        else{
+            alert("You cannot afford an autoclicker yet!")
+        }
+    //TODO add functionality to the autoclick
     }
-    multiplierFunct(){
-        this._count * 2; 
+    multiplierClick() {
+        if(this._count >= this._multiplierCost){
+            this._count -= this._multiplierCost;
+            this._multiplierCost *= 1.1;
+            this._multiplier++;
+            this._clickValue *= 1.2;
+        }
+            else{
+                alert("You cannot afford a multiplier yet!")
+            }
+        }
     }
-}
+
+
+    // setInterval(() => {
+    //     donut.click();
+    //     countEl.innerText = donut.count();
+    // }, 1000);
+    // getStatus(){
+    //     return "Donuts: " + this._count + " Auto-Clickers: " + this.autoclick + " Multiplier: " + this._multiplier;
+    // }
+    // multiplierFunct(){
+    //     this._count * 2; 
+    // }
+
 export default Donuts;
--- app.js
+++ app.js
@@ -5,19 +5,53 @@ import Donuts from "./Donuts.js";
 
 const countEl = document.querySelector("#count");
 const autoclickEl = document.querySelector("#autoclick");
-const multiplierEl = document.querySelector("#multiplier")
+const multiplierEl = document.querySelector("#multiplier");
+const autoclickCostEl = document.querySelector("#autoclickCost");
+const multiplierCostEl = document.querySelector("#multiplierCost");
 // const getStatusEl = document.querySelector("#status")
 
-const donut = new Donuts(1,1,1);
+const donut = new Donuts(220, 0, 0);
 
 countEl.innerText = donut._count;
 autoclickEl.innerText = donut._autoclick;
 multiplierEl.innerText = donut._multiplier;
+autoclickCostEl.innerText = donut._autoclickCost;
+multiplierCostEl.innerText = donut._multiplierCost;
 
-// StatusEl.innerHTML = donut.getStatus();
 
 const countBtn = document.querySelector("#countBtn");
 countBtn.addEventListener("click", () => {
   donut.click();
-  countEl.innerText = donut._count;
+  countEl.innerText = Math.round(donut._count);
 })
+
+
+const autoclickBtn = document.querySelector("#autoclickBtn");
+autoclickBtn.addEventListener("click", () => {
+  if (donut._count >= donut._autoclickCost) {
+    donut._count -= donut._autoclickCost;
+    donut._autoclickCost *= 1.1;
+    donut._autoclick++;
+    setInterval(() => {
+      donut.click();
+      countEl.innerText = Math.round(donut._count);
+      autoclickEl.innerText = Math.round(donut._autoclick)
+      autoclickCostEl.innerText = Math.round(donut._autoclickCost);
+    }, 1000)
+  }
+  else {
+    alert("You cannot afford an autoclicker yet!")
+  }
+})
+
+const multiplierBtn = document.querySelector("#multiplierBtn")
+multiplierBtn.addEventListener("click", () => {
+
+  donut.multiplierClick();
+  multiplierEl.innerText = Math.round(donut._multiplier)
+  multiplierCostEl.innerText = Math.round(donut._multiplierCost);
+  countEl.innerText = Math.round(donut._count)
+})
+
+
+
